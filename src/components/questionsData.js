482f29604f59a422d43bcadb240ebec9c2f8a544
so0733 <so0733@naver.com>
2023-12-10 01:15:21 +0900

    내용추가

--- src/components/questionsData.js
+++ src/components/questionsData.js
@@ -1831,6 +1831,7 @@ const questions = [
     // 2023년 3회
     {
         id: 241,
+        rotation: '2023년 3회',
         question: '다음은 자바 소스 코드이다. 출력 결과를 쓰시오.',
         answer: 'BDCDD',
         imageSize: { width: '350px', height: '350px' },
@@ -1838,18 +1839,21 @@ const questions = [
       },
       {
         id: 242,
+        rotation: '2023년 3회',
         question: '다음에서 설명하는 용어를 쓰시오.',
         detail: "- 사용자가 비밀번호를 제공하지 않고 다른 웹사이트나 애플리케이션의 접근 권한을 부여할 수 있게 하는 개방형 표준기술이다.\n\n- 구글, 페이스북 등의 외부 계정을 기반으로 토큰을 이용하여 간편하게 회원가입 및 로그인할 수 있는 기술이다.",
         answer: 'OAuth',
       },
       {
         id: 243,
+        rotation: '2023년 3회',
         question: '리눅스(Linux)에서 사용자에게 읽기/쓰기/실행 권한을 부여하고, 그룹에게는 읽기/실행을 부여하고, 그 이외에는 실행 권한을 Test.txt 파일에 부여하는 위한 명령어는 다음과 같다. 빈칸에 들어갈 답을 작성하시오. (8진법을 사용)',
         detail:"(  ①  ) (  ②  ) Test.txt",
         answer: '① chmod ② 751',
       },
       {
         id: 244,
+        rotation: '2023년 3회',
         question: '다음은 C언어 소스 코드이다. 출력 결과를 쓰시오.',
         answer: '34',
         imageSize: { width: '350px', height: '400px' },
@@ -1857,17 +1861,20 @@ const questions = [
       },
       {
         id: 245,
+        rotation: '2023년 3회',
         question: 'C언어에서 구조체의 멤버에 접근하기 위한 기호를 쓰시오.',
         answer: '->',
       },
       {
         id: 246,
+        rotation: '2023년 3회',
         question: '다음 빈칸에 들어갈 UNION 연산에 대한 결괏값을 작성하시오.',
         detail:"[쿼리]\nSELECT A FROM t1\nUNION\nSELECT A FROM t2\nORDER BY A DESC;\n\n[t1 테이블]\nA\n3\n1\n4\n\n[t2 테이블]\nA\n2\n4\n3",
         answer: '\nA\n4\n3\n2\n1',
       },
       {
         id: 247,
+        rotation: '2023년 3회',
         question: '다음은 서버 접근통제의 유형이다. 괄호( ) 안에 들어갈 용어를 쓰시오. (단, 영어 약어로 작성하시오.)',
         detail: "- (   ①   )은/는 규칙 기반(Rule-Based) 접근통제 정책으로, 객체에 포함된 정보의 허용 등급과 접근 정보에 대하여 주체가 갖는 접근 허가 권한에 근거하여 객체에 대한 접근을 제한하는 방법이다.\n\n " +
                 "- (   ②   )은/는 중앙 관리자가 사용자와 시스템의 상호관계를 통제하며 조직 내 맡은 역할(Role)에 기초하여 자원에 대한 접근을 제한하는 방법이다.\n\n " +
@@ -1876,6 +1883,7 @@ const questions = [
       },
       {
         id: 248,
+        rotation: '2023년 3회',
         question: '다음은 C언어 소스 코드이다. 출력 결과를 쓰시오.',
         answer: '5040',
         imageSize: { width: '350px', height: '400px' },
@@ -1883,11 +1891,13 @@ const questions = [
       },
       {
         id: 249,
+        rotation: '2023년 3회',
         question: '프로그래밍에서 사용되는 특정한 데이터 구조의 Cell 크기가 53 바이트이며, 이 데이터 구조를 이용한 연산이 비동기적으로 수행된다. 이 데이터 구조는 주로 어떤 용도로 사용되며, 그에 따른 약어는?',
         answer: 'ATM',
       },
       {
         id: 250,
+        rotation: '2023년 3회',
         question: '다음은 C언어 소스 코드이다. 출력 결과를 쓰시오.',
         answer: '\nKOREA\nOREA\nK\nE\nO',
         imageSize: { width: '350px', height: '400px' },
@@ -1895,6 +1905,7 @@ const questions = [
       },
       {
         id: 251,
+        rotation: '2023년 3회',
         question: '다음은 자바 소스 코드이다. 출력 결과를 쓰시오.',
         answer: '2',
         imageSize: { width: '350px', height: '400px' },
@@ -1902,11 +1913,13 @@ const questions = [
       },
       {
         id: 252,
+        rotation: '2023년 3회',
         question: '사설 네트워크에 속한 IP를 공인 IP 주소로 바꿔 주는 네트워크 주소 변환 기술은 무엇인지 쓰시오.',
         answer: 'NAT',
       },
       {
         id: 253,
+        rotation: '2023년 3회',
         question: '다음은 자바 소스 코드이다. 오류가 발생하는 코드 라인 번호를 쓰시오.',
         answer: '7',
         imageSize: { width: '350px', height: '400px' },
@@ -1914,12 +1927,14 @@ const questions = [
       },
       {
         id: 254,
+        rotation: '2023년 3회',
         question: '다음은 파이썬 소스 코드이다. 빈칸에 들어갈 함수를 쓰시오.',
         detail: "num1, num2 = input().(_______)",
         answer: 'split',
       },
       {
         id: 255,
+        rotation: '2023년 3회',
         question: '다음은 어떤 UML 다이어그램에 관한 예시이다. 어떤 종류의 다이어그램인가?',
         answer: '패키지',
         imageSize: { width: '350px', height: '400px' },
@@ -1927,11 +1942,13 @@ const questions = [
       },
       {
         id: 256,
+        rotation: '2023년 3회',
         question: '입력 데이터의 영역을 유사한 도메인별로 유효값/무효값을 그룹핑하여 대푯값 테스트 케이스를 도출하여 테스트하는 기법은 무엇인지 영어로 쓰시오.',
         answer: 'Equivalence Partitioning',
       },
       {
         id: 257,
+        rotation: '2023년 3회',
         question: '클라우드 유형을 다음 그림과 같이 나누었을 때 각각 알맞은 것을 보기에서 고르시오.',
         detail: "[보기] IaaS, PaaS, SaaS",
         answer: '① IaaS ② PaaS ③ SaaS',
@@ -1940,17 +1957,20 @@ const questions = [
       },
       {
         id: 258,
+        rotation: '2023년 3회',
         question: 'IGP 중 거리벡터 알고리즘, 라우팅 프로토콜에 해당하는 프로토콜을 쓰시오.',
         answer: 'RIP',
       },
       {
         id: 259,
+        rotation: '2023년 3회',
         question: '다음 보기에서 각각 알맞은 기호를 찾아 쓰시오.',
         detail: "① 조인(join) \n② 프로젝트(project) \n③ 시그마(select) \n④ 나누기(division)",
         answer: '\n① ⋈\n② π \n③ σ \n④ ÷',
       },
       {
         id: 260,
+        rotation: '2023년 3회',
         question: '다음 릴레이션에 생기는 문제를 방지하기 위해서는 (       )무결성 제약 조건을 준수해야 한다. 괄호 (   ) 안에 들어갈 용어를 쓰시오.',
         detail: "- 릴레이션 위, 아래 하나씩 있는데 밑에 릴레이션에서 A라는 고객이 위에 릴레이션에 없음",
         answer: '참조',
